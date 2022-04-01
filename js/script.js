bbd59177c405c282783863e5ee38b7c98a6da3c8
Davina Leong <davinaleong@firstcom.com.sg>
2022-04-01 11:01:20 +0800

    Able to toggle main menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Script loaded.");
+// console.log("Script loaded.");
+const mainHeader = document.getElementById("mainHeader");
+const toggleNav = document.getElementById("toggleNav");
+const classExpanded = "expanded";
+
+toggleNav.addEventListener("click", (e) => {
+  mainHeader.classList.toggle(classExpanded);
+});
