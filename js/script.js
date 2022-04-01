73e45ec78c445837c5e5afbf6fd3cc1804bb48e4
Davina Leong <davinaleong@firstcom.com.sg>
2022-04-01 11:36:25 +0800

    Added padding to body

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// console.log("Script loaded.");
+const body = document.querySelector("body");
+
 const mainHeader = document.getElementById("mainHeader");
 const toggleNav = document.getElementById("toggleNav");
 const classExpanded = "expanded";
@@ -8,13 +9,14 @@ const copyright = document.getElementById("copyright");
 const createdDate = new Date("2022-03-23");
 const now = new Date();
 
+// Nav toggle
 toggleNav.addEventListener("click", (e) => {
   mainHeader.classList.toggle(classExpanded);
 });
 
+// Print copyright
 let dateText = now.getFullYear();
 if (now.getFullYear() !== createdDate.getFullYear()) {
   dateText = `${createdDate.getFullYear()} - ${now.getFullYear()}`;
 }
-console.log(dateText);
 copyright.innerText = `Handcrafted by Davina Leong, ${dateText}.`;
