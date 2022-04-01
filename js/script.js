6f63e499aa26b13afc48cf5fb896a8a37c745be4
Davina Leong <davinaleong@firstcom.com.sg>
2022-04-01 11:22:49 +0800

    Added footer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ const mainHeader = document.getElementById("mainHeader");
 const toggleNav = document.getElementById("toggleNav");
 const classExpanded = "expanded";
 
+const copyright = document.getElementById("copyright");
+
+const createdDate = new Date("2022-03-23");
+const now = new Date();
+
 toggleNav.addEventListener("click", (e) => {
   mainHeader.classList.toggle(classExpanded);
 });
+
+let dateText = now.getFullYear();
+if (now.getFullYear() !== createdDate.getFullYear()) {
+  dateText = `${createdDate.getFullYear()} - ${now.getFullYear()}`;
+}
+console.log(dateText);
+copyright.innerText = `Handcrafted by Davina Leong, ${dateText}.`;
