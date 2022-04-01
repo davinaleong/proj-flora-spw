9ab7bcc5e374be8af5813135cbdb022daafdd424
Davina Leong <davinaleong@firstcom.com.sg>
2022-04-01 14:15:31 +0800

    Added about section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+const main = document.querySelector("main");
 
 const mainHeader = document.getElementById("mainHeader");
 const toggleNav = document.getElementById("toggleNav");
@@ -8,6 +9,10 @@ const copyright = document.getElementById("copyright");
 
 const createdDate = new Date("2022-03-23");
 const now = new Date();
+
+main.addEventListener("click", (e) => {
+  mainHeader.classList.remove(classExpanded);
+});
 
 // Nav toggle
 toggleNav.addEventListener("click", (e) => {
@@ -19,4 +24,4 @@ let dateText = now.getFullYear();
 if (now.getFullYear() !== createdDate.getFullYear()) {
   dateText = `${createdDate.getFullYear()} - ${now.getFullYear()}`;
 }
-copyright.innerText = `Handcrafted by Davina Leong, ${dateText}.`;
+copyright.innerText = `Handcrafted by Davina Leong, ${dateText}`;
