9c7b7a327062a252ee88ca7645a8adcd449d671d
Davina Leong <davinaleong@firstcom.com.sg>
2022-04-01 16:46:24 +0800

    Done with mobile version

--- js/script.js
+++ js/script.js
@@ -1,27 +1,68 @@
-const body = document.querySelector("body");
-const main = document.querySelector("main");
-
-const mainHeader = document.getElementById("mainHeader");
-const toggleNav = document.getElementById("toggleNav");
 const classExpanded = "expanded";
+const classHidden = "hidden";
 
-const copyright = document.getElementById("copyright");
+window.onload = () => {
+  const main = document.querySelector("main");
+  const mainHeader = document.getElementById("mainHeader");
+  const toggleNav = document.getElementById("toggleNav");
 
-const createdDate = new Date("2022-03-23");
-const now = new Date();
+  const categoryButtons = document.querySelectorAll(
+    "button[data-click=modalTrigger]"
+  );
 
-main.addEventListener("click", (e) => {
-  mainHeader.classList.remove(classExpanded);
-});
+  const closeButtons = document.querySelectorAll(".close");
 
-// Nav toggle
-toggleNav.addEventListener("click", (e) => {
-  mainHeader.classList.toggle(classExpanded);
-});
+  // Collapse nav when main is clicked
+  main.addEventListener("click", (e) => {
+    mainHeader.classList.remove(classExpanded);
+  });
 
-// Print copyright
-let dateText = now.getFullYear();
-if (now.getFullYear() !== createdDate.getFullYear()) {
-  dateText = `${createdDate.getFullYear()} - ${now.getFullYear()}`;
-}
-copyright.innerText = `Handcrafted by Davina Leong, ${dateText}`;
+  // Toggle nav when toggle is clicked
+  toggleNav.addEventListener("click", (e) => {
+    mainHeader.classList.toggle(classExpanded);
+  });
+
+  printCopyright();
+  hideModals();
+
+  categoryButtons.forEach((button) => {
+    button.addEventListener("click", (e) => showSingleModal(e.target));
+  });
+
+  closeButtons.forEach((button) => {
+    button.addEventListener("click", (e) => hideSingleModal(e.target));
+  });
+};
+
+// Show copyright with year on footer
+const printCopyright = () => {
+  const copyright = document.getElementById("copyright");
+  const createdDate = new Date("2022-03-23");
+  const now = new Date();
+
+  let dateText = now.getFullYear();
+  if (now.getFullYear() !== createdDate.getFullYear()) {
+    dateText = `${createdDate.getFullYear()} - ${now.getFullYear()}`;
+  }
+  copyright.innerText = `Handcrafted by Davina Leong, ${dateText}`;
+};
+
+// Hide all modals
+const hideModals = () => {
+  const modals = document.querySelectorAll(".modal");
+
+  modals.forEach((modal) => modal.classList.add(classHidden));
+};
+
+// Show modal when category is clicked
+const showSingleModal = (target) => {
+  const selector = target.dataset.target;
+  const modal = document.querySelector(selector);
+  if (modal) {
+    modal.classList.remove(classHidden);
+  }
+};
+
+const hideSingleModal = (target) => {
+  target.parentNode.parentNode.classList.add(classHidden);
+};
